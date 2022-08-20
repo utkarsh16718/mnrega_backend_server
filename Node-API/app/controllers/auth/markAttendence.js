13f55c8176ed5d68928327b4f3098a49f30cb479
utkarsh16718 <utkarsh.raj.16718@gmail.com>
2022-08-20 19:42:29 +0530

    Update markAttendence.js

--- Node-API/app/controllers/auth/markAttendence.js
+++ Node-API/app/controllers/auth/markAttendence.js
@@ -19,33 +19,33 @@ const markAttendence = async (req, res) => {
 
         const attendanceId = crypto.randomBytes(16).toString("hex");
 
-        // The path to your python script
-        var myPythonScript = "C:/Users/utkar/Desktop/Backend_Server/ML_Model/manregaAwsManagement/AWSManagement/getModelPathAndReturnListofIds.py";
-        // Provide the path of the python executable, if python is available as environment variable then you can use only "python"
-        var pythonExecutable = "python.exe";
+        // // The path to your python script
+        // var myPythonScript = "C:/Users/utkar/Desktop/Backend_Server/ML_Model/manregaAwsManagement/AWSManagement/getModelPathAndReturnListofIds.py";
+        // // Provide the path of the python executable, if python is available as environment variable then you can use only "python"
+        // var pythonExecutable = "python.exe";
 
-        // Function to convert an Uint8Array to a string
-        var uint8arrayToString = function (data) {
-            return String.fromCharCode.apply(null, data);
-        };
+        // // Function to convert an Uint8Array to a string
+        // var uint8arrayToString = function (data) {
+        //     return String.fromCharCode.apply(null, data);
+        // };
 
-        const spawn = require('child_process').spawn;
-        const scriptExecution = spawn(pythonExecutable, [myPythonScript, req.body.groupPhotoUrl,attendanceId]);
+        // const spawn = require('child_process').spawn;
+        // const scriptExecution = spawn(pythonExecutable, [myPythonScript, req.body.groupPhotoUrl,attendanceId]);
 
-        // Handle normal output
-        scriptExecution.stdout.on('data', (data) => {
-            console.log(uint8arrayToString(data));
-        });
+        // // Handle normal output
+        // scriptExecution.stdout.on('data', (data) => {
+        //     console.log(uint8arrayToString(data));
+        // });
 
-        // Handle error output
-        scriptExecution.stderr.on('data', (data) => {
-            // As said before, convert the Uint8Array to a readable string.
-            console.log(uint8arrayToString(data));
-        });
+        // // Handle error output
+        // scriptExecution.stderr.on('data', (data) => {
+        //     // As said before, convert the Uint8Array to a readable string.
+        //     console.log(uint8arrayToString(data));
+        // });
 
-        scriptExecution.on('exit', (code) => {
-            console.log("Process quit with code : " + code);
-        });
+        // scriptExecution.on('exit', (code) => {
+        //     console.log("Process quit with code : " + code);
+        // });
 
 
         const { supervisorId, WorkLocation, workId, groupPhotoUrl } = req.body;
